22e5ffd6f6a580dd2bde3e54b5148b3f0b0f8bf9
jennri <121411741+jennri@users.noreply.github.com>
2023-06-06 02:03:56 +1000

    working images

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ search.addEventListener('click', e => {
     
     clearContent()
 
-
     let cityValue = city.value.toLowerCase();
     getWeatherData(cityValue)
     city.value = null;
@@ -17,16 +16,19 @@ search.addEventListener('click', e => {
 })
 
 function clearContent(){
-    const content = document.querySelector("#content")
-    const pageContent = document.querySelector(".page-content");
-    const detailsContent = document.querySelector(".details-content")
+    const content = document.querySelector('#content')
+    const pageContent = document.querySelector('.page-content');
+    const detailsContent = document.querySelector('.details-content')
+    const weatherContainer = document.querySelector('.weather-content')
     if (pageContent){
         content.removeChild(pageContent)
         content.removeChild(detailsContent)
+        content.removeChild(weatherContainer)
     }
 }
 
 async function getWeatherData(cityValue) {
+
     //All of this is how to retrieve data using API
     //Fetch takes the API Key and the city input, followed by two .then funcitons
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=`+key()+`&q=`+cityValue, {
@@ -39,58 +41,33 @@ async function getWeatherData(cityValue) {
         const pageContent = document.createElement('div')
         pageContent.classList.add('page-content')
     
-
-
         const displayCityName = document.createElement('h2')
         displayCityName.textContent = response.location.name + ", " + response.location.country
         pageContent.appendChild(displayCityName)
 
+
+        const weatherContent = document.createElement('div')
+        const weatherIcon = document.createElement('div')
+
+        const weatherContainer = document.createElement('div')
+        weatherContainer.classList.add('weather-content')
+
+        let iconValue = response.current.condition.icon.slice(-7, -4)
+
+        const imgg = new Image(100, 100)
+        imgg.setAttribute("src", "E:/Programming Learning/Git Repos/TOP-WeatherApp/dist/img/day/" + iconValue + ".png")
+        weatherIcon.appendChild(imgg)
+
         const feelsTempC = document.createElement('p')
         feelsTempC.textContent = "Feels like " + response.current.feelslike_c + "°C"
-        pageContent.appendChild(feelsTempC)
+        weatherContent.appendChild(feelsTempC)
 
         const tempC = document.createElement('h1')
         tempC.textContent = response.current.temp_c + "°C"
-        pageContent.appendChild(tempC)
+        weatherContent.appendChild(tempC)
 
-        const img = document.createElement('h2')
-        img.textContent = response.current.condition.text
-        pageContent.appendChild(img)
-
-        // const imgg = document.createElement(img)
-        // imgg.src = response.current.condition.icon
-        // pageContent.appendChild(imgg)
-
-        console.log(response)
-        // switch (response.current.condition){
-        //     case 'Sunny':
-        //         image.src = 'images/clear.png';
-        //         break;
-
-        //     case 'Rain':
-        //         image.src = 'images/rain.png';
-        //         break;
-
-        //     case 'Snow':
-        //         image.src = 'images/snow.png';
-        //         break;
-
-        //     case 'Partly cloudy':
-        //         image.src = 'images/cloud.png';
-        //         break;
-
-        //     case 'Haze':
-        //         image.src = 'images/mist.png';
-        //         break;
-
-        //     default:
-        //         image.src = '';
-        // }  
-
-        const uv = document.createElement('h3')
-        uv.textContent = 'UV ' + response.current.uv
-        pageContent.appendChild(uv)
-
+        weatherContainer.appendChild(weatherIcon)
+        weatherContainer.appendChild(weatherContent)
 
         const detailsContent = document.createElement('div')
         detailsContent.classList.add('details-content')
@@ -100,14 +77,18 @@ async function getWeatherData(cityValue) {
         humidity.textContent = 'Humidity ' + response.current.humidity + " %"
         detailsContent.appendChild(humidity)
 
+        const uv = document.createElement('h3')
+        uv.textContent = 'UV ' + response.current.uv
+        detailsContent.appendChild(uv)
+
         const windSpeed = document.createElement('h3')
         windSpeed.setAttribute('class', 'contentDetailsSpeed')
         windSpeed.textContent = response.current.wind_kph + ' kp/h'
         detailsContent.appendChild(windSpeed)
 
         content.appendChild(pageContent)
+        content.appendChild(weatherContainer)
         content.appendChild(detailsContent)
-    
     })
  
 }
